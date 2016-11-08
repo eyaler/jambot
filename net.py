a498185a474e06df0f29c7f81be3173ed2ef1f1f
eyaler <eyalgruss@gmail.com>
2016-11-08 16:37:12 +0200

    bugfix

--- net.py
+++ net.py
@@ -11,7 +11,7 @@ def get_train_model(max_len):
     input = Input(shape=(max_len, octave_len, states))
     a = Reshape([max_len, octave_len*states])(input)
     a = TimeDistributed(Dense(embedding_size))(a)
-    for l in lstm_layers:
+    for i in range(lstm_layers):
         a = LSTM(lstm_size, return_sequences=True)(a)
     a = TimeDistributed(Dense(octave_len*states))(a)
     a = Reshape([max_len, octave_len, states])(a)
@@ -22,7 +22,7 @@ def get_test_model():
     input = Input(batch_shape=(1, 1, octave_len, states))
     a = Reshape([1, octave_len*states])(input)
     a = TimeDistributed(Dense(embedding_size))(a)
-    for l in lstm_layers:
+    for i in range(lstm_layers):
         a = LSTM(lstm_size, stateful=True)(a)
     a = Dense(octave_len*states, name='timedistributed_3')(a)
     a = Reshape([octave_len, states])(a)
@@ -40,7 +40,7 @@ def get_train_model1(max_len):
 
     a = merge([a1,a2], mode='concat')
 
-    for l in lstm_layers:
+    for i in range(lstm_layers):
         a = LSTM(lstm_size, return_sequences=True)(a)
     a = TimeDistributed(Dense(octave_len*states))(a)
     a = Reshape([max_len, octave_len, states])(a)
@@ -58,7 +58,7 @@ def get_test_model1():
 
     a = merge([a1, a2], mode='concat')
 
-    for l in lstm_layers:
+    for i in range(lstm_layers):
         a = LSTM(lstm_size, stateful=True)(a)
     a = Dense(octave_len*states, name='timedistributed_3')(a)
     a = Reshape([octave_len, states])(a)
