a78a14dee5bfcb5ed12772f8a78d50082c1837a0
talbaumel <talbaumel@gmail.com>
2016-11-08 14:40:00 +0200

    Added sample data

--- parser2/main.py
+++ parser2/main.py
@@ -1,8 +1,11 @@
-from gp_file_yielder import yield_gp_files
-from parse_gp_file import parse_gp_file
-from config import *
+import guitarpro
 
 if __name__ == '__main__':
+<<<<<<< Updated upstream
     for file_name, artist in yield_gp_files(gp_folder):
         for guitar_part, bass_part in parse_gp_file(file_name):
             pass
+=======
+    gp_file = guitarpro.parse('../sample_data/311/311 - All Mixed Up.gp3')
+    2
+>>>>>>> Stashed changes
